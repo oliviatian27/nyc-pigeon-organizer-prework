e4da428ee4a7e24a53938905724b6b3ed6c719a9
Olivia Tian <oliviatian27@gmail.com>
2018-07-25 16:13:12 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  result=Hash.new
+   data.each do |feature,detail|
+     detail.each do |key,value|
+       value.each do |x|
+         if result.has_key?(x)
+            if result[x].has_key?(feature)
+               result[x][feature] << key.to_s
+            else
+               result[x][feature]=[]
+               result[x][feature] << key.to_s
+            end
+         else
+            result[x]={}
+            result[x][feature]=[]
+            result[x][feature] << key.to_s
+         end
+        end
+      end
+     end 
+   result      
 end
