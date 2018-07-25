6ee13539fccd8b1367f076ffedfac6ae4611bdca
Olivia Tian <oliviatian27@gmail.com>
2018-07-25 15:50:58 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,10 @@
 def nyc_pigeon_organizer(data)
-   data.each do |color,
+  result=Hash.new(0)
+   data.each do |feature,detail|
+     detail.each do |key,value|
+       value.each do |x|
+         
+         result[x][feature]=[]
+         result[x][feature] << key
+         
 end
